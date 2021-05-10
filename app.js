d6f303ee3be511d9b7909b02ff7affcf4f2cd24c
stormymayday <stormymayday7@gmail.com>
2021-05-09 22:55:14 -0600

    Basic GraphQL API Implementation

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // Importing Express
 const express = require('express');
+// Importing Express GraphQL
+const { graphqlHTTP } = require('express-graphql');
+// Importing buildSchema property
+const { buildSchema } = require('graphql');
 
 // Creating app object by calling Express
 const app = express();
@@ -7,10 +11,34 @@ const app = express();
 //Body-Parsing
 app.use(express.json());
 
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+
+        type RootQuery {
+            farmers: [String!]!
+        }
+
+        type RootMutation {
+            createFarmer(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+
+    `),
+    rootValue: {
+        farmers: () => {
+            return ['Claudia & Juan', 'Mirian Vasquez'];
+        },
+        createFarmer: (args) => {
+            const farmerName = args.name;
+            return farmerName;
+        }
+    },
+    graphiql: true
+}));
+
 // Listening on port 3000
 app.listen(3000);
-
-// Server Test
-app.get('/', (req, res, next) => {
-    res.send('Hello World!');
-})
